5917e967f33c059fcc108e63ae22acc237860b91
anil <anilsonone@gmail.com>
2022-12-23 22:05:28 +0530

    Initial Commit

---
+++ src/main/java/com/anil/theatre/OnlineTheatreBooking1Application.java
@@ -0,0 +1,13 @@
+package com.anil.theatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTheatreBooking1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTheatreBooking1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anil/theatre/OnlineTheatreBooking1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.theatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTheatreBooking1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
